316ecd745fcef4458af48484471d29767db9afec
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-08-30 13:43:17 +0200

    fix: add .js extension for lodash imports for esm usage

--- src/hooks/setData.ts
+++ src/hooks/setData.ts
@@ -1,6 +1,6 @@
-import _get from "lodash/get";
-import _set from "lodash/set";
-import _has from "lodash/has";
+import _get from "lodash/get.js";
+import _set from "lodash/set.js";
+import _has from "lodash/has.js";
 
 import { Forbidden } from "@feathersjs/errors";
 import { getItemsIsArray } from "../utils/getItemsIsArray";
