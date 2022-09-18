cefd493d8f47f9209c615cb33062c802a7d71445
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-09-18 20:24:07 +0200

    feat(filters): add filterObject

--- src/filters/array.ts
+++ src/filters/array.ts
@@ -1,9 +1,9 @@
 import type { FilterQueryOptions } from "@feathersjs/adapter-commons";
 import { validateQueryProperty } from "../utils/validateQueryProperty";
 
-export const filterArray = () => (arr: any, { operators }: FilterQueryOptions) => {
+export const filterArray = (key: string) => (arr: any, { operators }: FilterQueryOptions) => {
   if (arr && !Array.isArray(arr)) {
-    throw new Error("Invalid query parameter $and. It has to be an array");
+    throw new Error(`Invalid query parameter '${key}'. It has to be an array`);
   }
 
   if (Array.isArray(arr)) {
