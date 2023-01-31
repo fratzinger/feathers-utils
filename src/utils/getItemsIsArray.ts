88396ccac6f9fd3b184204be67c2e057d745bc2e
fratzinger <22286818+fratzinger@users.noreply.github.com>
2023-01-31 14:29:59 +0100

    feat(getItemsIsArray): explicit 'data' | 'result' | 'automatic'

--- src/utils/getItemsIsArray.ts
+++ src/utils/getItemsIsArray.ts
@@ -1,16 +1,34 @@
 import type { HookContext } from "@feathersjs/feathers";
-import type { GetItemsIsArrayOptions } from "..";
+import type { GetItemsIsArrayResult } from "..";
+
+export type GetItemsIsArrayOptions = {
+  from: "data" | "result" | "automatic"
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getItemsIsArray = <T = any>(
-  context: HookContext
-): GetItemsIsArrayOptions<T> => {
-  let itemOrItems = context.type === "before" 
-    ? context.data 
-    : context.result;
-  itemOrItems = itemOrItems && context.method === "find" 
-    ? (itemOrItems.data || itemOrItems) 
-    : itemOrItems;
+  context: HookContext,
+  options?: GetItemsIsArrayOptions
+): GetItemsIsArrayResult<T> => {
+  const {
+    from = "automatic"
+  } = options || {};
+
+  let itemOrItems;
+
+  if (from === "automatic") {
+    itemOrItems = context.type === "before" 
+      ? context.data 
+      : context.result;
+    itemOrItems = itemOrItems && context.method === "find" 
+      ? (itemOrItems.data || itemOrItems) 
+      : itemOrItems;
+  } else if (from === "data") {
+    itemOrItems = context.data;
+  } else if (from === "result") {
+    itemOrItems = context.result;
+  }
+  
   const isArray = Array.isArray(itemOrItems);
   return {
     items: (isArray) 
