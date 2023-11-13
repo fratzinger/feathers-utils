826bca351fcefa4bffe71a30b3e892624e9752cb
fratzinger <22286818+fratzinger@users.noreply.github.com>
2023-11-13 09:06:21 +0100

    fix(type): generic return

--- src/hooks/forEach.ts
+++ src/hooks/forEach.ts
@@ -14,14 +14,14 @@ export const forEach = <H extends HookContext = HookContext, T = any>(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   actionPerItem: (item: T, context: H) => Promisable<any>,
   _options?: HookForEachOptions,
-): ReturnAsyncHook => {
+): ReturnAsyncHook<H> => {
   const options: Required<HookForEachOptions> = {
     wait: "parallel",
     items: "automatic",
     ..._options,
   };
 
-  return async (context: HookContext): Promise<HookContext> => {
+  return async (context: H): Promise<H> => {
     if (shouldSkip("runForItems", context)) {
       return context;
     }
