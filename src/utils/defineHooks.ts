2035ab73c6bd469802db31b85825afcfb1a5be7a
fratzinger <22286818+fratzinger@users.noreply.github.com>
2023-11-23 19:50:49 +0100

    feat(utils): new util - defineHooks (#21)

---
+++ src/utils/defineHooks.ts
@@ -0,0 +1,16 @@
+import type { Application, HookOptions } from "@feathersjs/feathers";
+
+export function defineHooks<
+  A extends Application = Application,
+  S = {
+    find: any;
+    get: any;
+    create: any;
+    update: any;
+    patch: any;
+    remove: any;
+  },
+  Options = HookOptions<A, S>,
+>(hooks: Options): Options {
+  return hooks;
+}
